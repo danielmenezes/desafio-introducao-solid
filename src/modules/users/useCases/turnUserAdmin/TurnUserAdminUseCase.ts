e42ab6b8c97bde44e3064343d1930fefebc1c199
danielmenezes <daniel.menezes.md@gmail.com>
2021-12-25 17:23:28 -0300

    test if user exists in use case turnAdmin

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -13,6 +13,10 @@ class TurnUserAdminUseCase {
   execute({ user_id }: IRequest): User {
     const user = this.usersRepository.findById(user_id);
 
+    if (!user) {
+      throw new Error("User not exists");
+    }
+
     return this.usersRepository.turnAdmin(user);
   }
 }
