e9331f81da47f74fa8e648adec326c41ec586027
danielmenezes <daniel.menezes.md@gmail.com>
2021-12-25 17:20:07 -0300

    added test if user already exists in use case for creating user

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -12,6 +12,12 @@ class CreateUserUseCase {
   }
 
   execute({ email, name }: IRequest): User {
+    const userAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
     const user = this.usersRepository.create({ email, name });
 
     return user;
